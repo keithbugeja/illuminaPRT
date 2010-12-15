5854bc8899633c77d04f5b3f960105b21dd04667
kbugeja_cp <SND\kbugeja_cp>
2010-12-15 17:27:52 +0000

    Slight fixes to Whitted Integrator

--- Utility/Sandbox/main.cpp
+++ Utility/Sandbox/main.cpp
@@ -977,7 +977,7 @@ void RayTracer(int p_nOMPThreads)
 	//----------------------------------------------------------------------------------------------
 	// Scene creation complete
 	//----------------------------------------------------------------------------------------------
-	PointLight pointLight(Vector3(0,5,0), RGBSpectrum(1,1,1));
+	PointLight pointLight(Vector3(0,5,0), RGBSpectrum(100,100,100));
  
 	Scene scene(&basicSpace);
 	scene.LightList.PushBack(&pointLight);
