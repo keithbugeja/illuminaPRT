bbdceb2c5288d22e7be5af808f4952d8e24724a4
kbugeja_cp <SND\kbugeja_cp>
2010-12-17 17:24:55 +0000

    --

--- Core/Integrator/Integrator.h
+++ Core/Integrator/Integrator.h
@@ -7,7 +7,6 @@
 #pragma once
 
 #include "System/IlluminaPRT.h"
-//#include "Geometry/Ray.h"
 //----------------------------------------------------------------------------------------------
 namespace Illumina
 {
@@ -20,6 +19,9 @@ namespace Illumina
 			virtual bool Shutdown(void) = 0;
 
 			virtual Spectrum Radiance(Scene *p_pScene, const Ray &p_ray, Intersection &p_intersection) = 0;
+
+			static Spectrum EstimateDirectLighting(Scene *p_pScene, ILight *p_pLight, 
+				const Vector3 &p_point, const Vector3 &p_normal, Vector3 &p_wOut);
 		};
 	}
 }
