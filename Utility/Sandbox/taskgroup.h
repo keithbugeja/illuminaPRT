4ab91e3fb58903b9f4a649bca7ad85cb84679fff
Keith Bugeja <keithbugeja@users.noreply.github.com>
2011-12-08 22:45:02 +0000

    --

--- Utility/Sandbox/taskgroup.h
+++ Utility/Sandbox/taskgroup.h
@@ -66,7 +66,7 @@ namespace Illumina
 				return GetRank() != MPI_UNDEFINED;
 			}
 
-			void UniversalBarrier(std::string& p_comment)
+			void UniversalBarrier(const std::string &p_comment)
 			{
 				int rank = GetUniversalRank();
 				std::cout << "[" << rank << "] :: Arrived at barrer :: [" << p_comment << "]" << std::endl;
@@ -74,27 +74,46 @@ namespace Illumina
 				std::cout << "[" << rank << "] :: Proceeded beyond barrer :: [" << p_comment << "]" << std::endl;
 			}
 
-			void Split(int p_splitCount)
+			void Split(int p_groupSize, bool p_distributeMasterRank = true)
 			{
-				int rank = GetUniversalRank();				
-				UniversalBarrier("Split");
+				this->UniversalBarrier("Range Split");
 
+				if (p_distributeMasterRank)
+				{
+					int rank = this->GetUniversalRank();
+					int masterRank = this->GetMasterRank();
+					int coordinatorRank = 0;
+
+					// We need to preserve zero (coodinator), 
+					// We need to go through task range to determine
+				}
+				else
+				{
+				}
+			}
+
+
+			void Split2(int p_splitCount)
+			{
+				this->UniversalBarrier("Split");
+
+				int rank = this->GetUniversalRank();				
 				std::cout << "[" << rank << "] :: Split :: [Group Size = " << Size <<", Split Count = " << p_splitCount << "]" << std::endl;		
 
 				// Determine rank zero in current group
-				int rankZero = this->GetRankZero();
+				int masterRank = this->GetMasterRank();
 		
 				// Rank zero is within specified range
-				if (rankZero > 0 && rankZero < p_splitSize)
+				if (masterRank > 0 && masterRank < p_splitCount)
 				{
-					int leftBound = rankZero - 1,
-						rightBound = p_splitSize;
+					int leftBound = masterRank - 1,
+						rightBound = p_splitCount;
 
 					int range[3][3];
 
 					range[0][0] = 0; range[0][1] = leftBound; range[0][2] = 1;
-					range[1][0] = rankZero + 1; range[1][1] = rightBound; range[1][2] = 1;
-					range[2][0] = rankZero, range[2][1] = rankZero; range[2][2] = 1;
+					range[1][0] = masterRank + 1; range[1][1] = rightBound; range[1][2] = 1;
+					range[2][0] = masterRank, range[2][1] = masterRank; range[2][2] = 1;
 
 					std::cout << "[" << rank << "] :: Split ranges :: [ " << 
 						range[0][0] << "," << range[0][1] << "," << range[0][2] << "," <<
@@ -108,9 +127,9 @@ namespace Illumina
 				{
 					int range[2][3];
 
-					if (!rankZero) { range[0][0] = 1; range[0][1] = p_splitSize; range[0][2] = 1; }
-					else { range[0][0] = 0; range[0][1] = p_splitSize - 1; range[0][2] = 1; }
-					range[1][0] = rankZero; range[1][1] = rankZero; range[1][2] = 1;
+					if (!masterRank) { range[0][0] = 1; range[0][1] = p_splitCount; range[0][2] = 1; }
+					else { range[0][0] = 0; range[0][1] = p_splitCount - 1; range[0][2] = 1; }
+					range[1][0] = masterRank; range[1][1] = masterRank; range[1][2] = 1;
 
 					std::cout << "[" << rank << "] :: Split ranges :: [ " << 
 						range[0][0] << "," << range[0][1] << "," << range[0][2] << "," <<
@@ -120,6 +139,7 @@ namespace Illumina
 					MPI_Group_range_excl(Group, 1, range, &Group);
 				}
 
+				this->UniversalBarrier("Split -> Created Groups");
 				std::cout << "[" << rank << "] :: " << std::hex << this->Group << ", " << p_subgroup->Group << std::dec << std::endl;
 
 				if (this->IsMember())
@@ -140,6 +160,7 @@ namespace Illumina
 				MPI_Group_size(p_subgroup->Group, &p_subgroup->Size);
 				MPI_Group_size(Group, &Size);
 
+				this->UniversalBarrier("Split -> Complete");
 				std::cout << "[" << rank << "] :: Split Successful :: [This size = " << Size << ", Split Size = " << p_subgroup->Size << "]" << std::endl;
 			}
 
