3213029c48bda888e63a5c9e0034ca58b5fb75b7
Keith Bugeja <keithbugeja@users.noreply.github.com>
2012-07-24 00:33:59 +0000

    Working on network streaming bug ... server intermittently displays unicast stream

--- Utility/Sandbox/main.cpp
+++ Utility/Sandbox/main.cpp
@@ -52,6 +52,370 @@ extern "C"
 //----------------------------------------------------------------------------------------------
 #if (!defined(TEST_SCHEDULER))
 //----------------------------------------------------------------------------------------------
+static void video_stream_example(const std::string& rtp_addr, int rtp_port, int codec_id)
+{
+	AVCodecContext *c = NULL;
+	AVCodec *codec;
+	AVFrame *picture;
+
+	int i, x, y, 
+		out_size, 
+		outbuf_size,
+		had_output = 0;
+
+	uint8_t *outbuf;
+
+	// find the video encoder
+	codec = avcodec_find_encoder((CodecID)codec_id);
+	
+	if (!codec) 
+	{
+		fprintf(stderr, "codec not found\n");
+		exit(1);
+	}
+
+	// get codec context and allocate frame
+	c = avcodec_alloc_context3(codec);
+	picture = avcodec_alloc_frame();
+
+	// put sample parameters
+	c->bit_rate = 400000;
+	
+	// resolution must be a multiple of two
+	c->width = 640;
+	c->height = 480;
+
+	// frames per second
+	c->time_base.num = 1; c->time_base.den = 25;
+	
+	c->gop_size = 10; /* emit one intra frame every ten frames */
+	c->max_b_frames = 25;
+	c->pix_fmt = PIX_FMT_YUV420P;
+
+	if(codec_id == CODEC_ID_H264)
+		av_opt_set (c->priv_data, "preset", "slow", 0);
+
+	// open codec
+	if (avcodec_open2(c, codec, NULL) < 0) 
+	{
+		fprintf(stderr, "could not open codec\n");
+		exit(1);
+	}
+
+	// allocate image and output buffer
+	outbuf_size = 100000 + 12 * c->width * c->height;
+	outbuf = (uint8_t*)malloc(outbuf_size);
+
+	
+	// the image can be allocated by any means and av_image_alloc() is
+	// just the most convenient way if av_malloc() is to be used */
+	av_image_alloc(picture->data, picture->linesize,
+				   c->width, c->height, c->pix_fmt, 1);
+
+
+
+	// initalize the AV context
+	AVFormatContext *oc = avformat_alloc_context();
+	if (!oc)
+	{
+		fprintf(stderr, "could not allocate format context");
+		exit(1);
+	}
+
+	// get the output format
+	AVOutputFormat *of = av_guess_format("rtp", NULL, NULL);
+	if (!of)
+	{
+		fprintf(stderr, "could guess output format");
+		exit(1);
+	}
+	
+	oc->oformat = of;
+
+	// try to open the RTP stream
+	sprintf(oc->filename, "rtp://%s:%d", rtp_addr.c_str(), rtp_port);
+	if (avio_open(&(oc->pb), oc->filename, AVIO_FLAG_WRITE) < 0)
+	{
+		fprintf(stderr, "could open rtp");
+		exit(1);
+	}
+
+	// add a stream
+	AVStream *s = av_new_stream(oc, 1);
+	if (!s)
+	{
+		fprintf(stderr, "could add stream");
+		exit(1);
+	}
+
+	// initalize codec
+	AVCodecContext* occ = s->codec;
+	occ->codec_id = c->codec_id;
+	occ->codec_type = c->codec_type;
+	occ->bit_rate = c->bit_rate;
+	occ->width = c->width;
+	occ->height = c->height;
+	occ->time_base.den = c->time_base.den;
+	occ->time_base.num = c->time_base.num;
+
+	// write the header
+	avformat_write_header(oc, NULL);
+
+
+	// encode video
+	for(i = 0;; i++) 
+	{
+		fflush(stdout);
+		// prepare a dummy image
+
+		for (y = 0; y < c->height; ++y)
+		{
+			for (x = 0; x < c->width; ++x)
+			{
+				picture->data[0][y * picture->linesize[0] + x] = (x * i) % 256;
+				
+				int x2 = x >> 1, y2 = y >> 1;
+
+				picture->data[1][y2 * picture->linesize[1] + x2] = i * (x + y) % 256;
+				picture->data[2][y2 * picture->linesize[2] + x2] = i * (x + y) % 128;
+			}
+		}
+
+		/* Y */
+		for(y=0;y<c->height;y++) 
+		{
+			for(x=0;x<c->width;x++) 
+			{
+				picture->data[0][y * picture->linesize[0] + x] = x + y + i * 3;
+			}
+		}
+
+		/* Cb and Cr */
+		for(y=0;y<c->height/2;y++) 
+		{
+			for(x=0;x<c->width/2;x++) 
+			{
+				picture->data[1][y * picture->linesize[1] + x] = 128 + y + i * 2;
+				picture->data[2][y * picture->linesize[2] + x] = 64 + x + i * 5;
+			}
+		}
+
+		// encode the image
+		out_size = avcodec_encode_video(c, outbuf, outbuf_size, picture);
+		had_output |= out_size;
+
+		// initalize a packet
+		AVPacket p;
+		av_init_packet(&p);
+		p.data = outbuf;
+		p.size = out_size;
+		p.stream_index = s->index;
+
+		// send it out
+		av_write_frame(oc, &p);
+	}
+
+	/* get the delayed frames */
+	for(; out_size || !had_output; i++) 
+	{
+		fflush(stdout);
+
+		out_size = avcodec_encode_video(c, outbuf, outbuf_size, NULL);
+		had_output |= out_size;
+
+		// initalize a packet
+		AVPacket p;
+		av_init_packet(&p);
+		p.data = outbuf;
+		p.size = out_size;
+		p.stream_index = s->index;
+
+		// send it out
+		av_write_frame(oc, &p);
+	}
+
+	// add sequence end code to have a real mpeg file
+	outbuf[0] = 0x00;
+	outbuf[1] = 0x00;
+	outbuf[2] = 0x01;
+	outbuf[3] = 0xb7;
+	//fwrite(outbuf, 1, 4, f);
+	free(outbuf);
+
+	avcodec_close(c);
+	av_free(c);
+	av_free(picture->data[0]);
+	av_free(picture);
+	printf("\n");
+}
+
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
+  FILE *pFile;
+  char szFilename[32];
+  int  y;
+  
+  // Open file
+  sprintf(szFilename, "frame%d.ppm", iFrame);
+  pFile=fopen(szFilename, "wb");
+  if(pFile==NULL)
+	return;
+  
+  // Write header
+  fprintf(pFile, "P6\n%d %d\n255\n", width, height);
+  
+  // Write pixel data
+  for(y=0; y<height; y++)
+	fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
+  
+  // Close file
+  fclose(pFile);
+}
+
+static void video_encode_example(const char *filename, int codec_id)
+{
+	AVCodecContext *c = NULL;
+	AVCodec *codec;
+	AVFrame *picture;
+
+	int i, x, y, 
+		out_size, 
+		outbuf_size,
+		had_output = 0;
+
+	uint8_t *outbuf;
+
+	FILE *f;
+
+	// Debug out
+	printf("Encode video file %s\n", filename);
+
+	// find the video encoder
+	codec = avcodec_find_encoder((CodecID)codec_id);
+	
+	if (!codec) 
+	{
+		fprintf(stderr, "codec not found\n");
+		exit(1);
+	}
+
+	// get codec context and allocate frame
+	c = avcodec_alloc_context3(codec);
+	picture = avcodec_alloc_frame();
+
+	// put sample parameters
+	c->bit_rate = 400000;
+	
+	// resolution must be a multiple of two
+	c->width = 640;
+	c->height = 480;
+
+	// frames per second
+	c->time_base.num = 1; c->time_base.den = 25;
+	
+	c->gop_size = 10; /* emit one intra frame every ten frames */
+	c->max_b_frames = 25;
+	c->pix_fmt = PIX_FMT_YUV420P;
+
+	if(codec_id == CODEC_ID_H264)
+		av_opt_set (c->priv_data, "preset", "slow", 0);
+
+	// open codec
+	if (avcodec_open2(c, codec, NULL) < 0) 
+	{
+		fprintf(stderr, "could not open codec\n");
+		exit(1);
+	}
+
+	// open file for writing
+	f = fopen(filename, "wb");
+	if (!f) 
+	{
+		fprintf(stderr, "could not open %s\n", filename);
+		exit(1);
+	}
+
+	// allocate image and output buffer
+	outbuf_size = 100000 + 12 * c->width * c->height;
+	outbuf = (uint8_t*)malloc(outbuf_size);
+
+	
+	// the image can be allocated by any means and av_image_alloc() is
+	// just the most convenient way if av_malloc() is to be used */
+	av_image_alloc(picture->data, picture->linesize,
+				   c->width, c->height, c->pix_fmt, 1);
+
+	// encode video
+	for(i = 0; i < 25 * 25; i++) 
+	{
+		fflush(stdout);
+		// prepare a dummy image
+
+		for (y = 0; y < c->height; ++y)
+		{
+			for (x = 0; x < c->width; ++x)
+			{
+				picture->data[0][y * picture->linesize[0] + x] = (x * i) % 256;
+				
+				int x2 = x >> 1, y2 = y >> 1;
+
+				picture->data[1][y2 * picture->linesize[1] + x2] = i * (x + y) % 256;
+				picture->data[2][y2 * picture->linesize[2] + x2] = i * (x + y) % 128;
+			}
+		}
+
+		/* Y */
+		for(y=0;y<c->height;y++) 
+		{
+			for(x=0;x<c->width;x++) 
+			{
+				picture->data[0][y * picture->linesize[0] + x] = x + y + i * 3;
+			}
+		}
+
+		/* Cb and Cr */
+		for(y=0;y<c->height/2;y++) 
+		{
+			for(x=0;x<c->width/2;x++) 
+			{
+				picture->data[1][y * picture->linesize[1] + x] = 128 + y + i * 2;
+				picture->data[2][y * picture->linesize[2] + x] = 64 + x + i * 5;
+			}
+		}
+
+		// encode the image
+		out_size = avcodec_encode_video(c, outbuf, outbuf_size, picture);
+		had_output |= out_size;
+		printf("encoding frame %3d (size=%5d)\n", i, out_size);
+		fwrite(outbuf, 1, out_size, f);
+	}
+
+	/* get the delayed frames */
+	for(; out_size || !had_output; i++) 
+	{
+		fflush(stdout);
+
+		out_size = avcodec_encode_video(c, outbuf, outbuf_size, NULL);
+		had_output |= out_size;
+		printf("write frame %3d (size=%5d)\n", i, out_size);
+		fwrite(outbuf, 1, out_size, f);
+	}
+
+	// add sequence end code to have a real mpeg file
+	outbuf[0] = 0x00;
+	outbuf[1] = 0x00;
+	outbuf[2] = 0x01;
+	outbuf[3] = 0xb7;
+	fwrite(outbuf, 1, 4, f);
+	fclose(f);
+	free(outbuf);
+
+	avcodec_close(c);
+	av_free(c);
+	av_free(picture->data[0]);
+	av_free(picture);
+	printf("\n");
+}
+
 void IlluminaPRT(bool p_bVerbose, int p_nIterations, std::string p_strScript)
 {
 	av_register_all();
@@ -144,7 +508,7 @@ void IlluminaPRT(bool p_bVerbose, int p_nIterations, std::string p_strScript)
 	}
 
 	//pRenderer->SetRenderBudget(1e+20f);
-	pRenderer->SetRenderBudget(0.25f / ((float)regions * 0.33f));
+	pRenderer->SetRenderBudget(0.05f / ((float)regions * 0.33f));
 	//pRenderer->SetRenderBudget(10.f);
 	Vector3 observer = pCamera->GetObserver();
 
@@ -164,7 +528,8 @@ void IlluminaPRT(bool p_bVerbose, int p_nIterations, std::string p_strScript)
 			////pCamera->MoveTo(Vector3(Maths::Cos(alpha) * lookFrom.X, lookFrom.Y, Maths::Sin(alpha) * lookFrom.Z));
 			////pCamera->LookAt(lookAt);
 			Vector3 observer_ = observer;
-			observer_.Z += Maths::Cos(alpha) * 200.f;
+			observer_.Z += Maths::Cos(alpha) * 3.f;
+			observer_.X += Maths::Sin(alpha) * 3.f;
 			pCamera->MoveTo(observer_);
 
 			// Start timer
@@ -196,7 +561,7 @@ void IlluminaPRT(bool p_bVerbose, int p_nIterations, std::string p_strScript)
 			}
 	 
 			// Render frame
-			#pragma omp parallel for num_threads(6)
+			#pragma omp parallel for schedule(static, 8) num_threads(4)
 			for (int y = 0; y < regionY; y++)
 			{
 				for (int x = 0; x < regionX; x++)
