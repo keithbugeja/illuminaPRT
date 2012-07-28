c87dc0c8b26a0bcbb39d1569121b06480ab6cb8a
Keith Bugeja <keithbugeja@users.noreply.github.com>
2012-07-28 10:34:50 +0000

    ...

--- Utility/Sandbox/TaskGroupController.h
+++ Utility/Sandbox/TaskGroupController.h
@@ -13,6 +13,23 @@
 #include "Logger.h"
 using namespace Illumina::Core;
 
+//----------------------------------------------------------------------------------------------
+//	TaskGroupState
+//----------------------------------------------------------------------------------------------
+class TaskGroupState
+{
+	std::vector<Task*> m_taskList;
+};
+
+//----------------------------------------------------------------------------------------------
+//	The task group controller should contain some state for the task group. This state shares
+//	generic information regarding the task group (but not its domain specific application and
+//	implementation) as well as task-specific details.
+//
+//	The controller is also reponsible of processing client requests / commands. Specialised
+//	command processors are applied to received commands, which are either parsed and passed
+//	on to the task group, or executed at the head node, or both.
+//----------------------------------------------------------------------------------------------
 class TaskGroupController
 {
 protected:
@@ -32,6 +49,11 @@ public:
 
 	bool Start(void) 
 	{
+		// Ok, now we wait for client to start sending commands... 
+		//	There is a number of command variants:
+		//		Some are executed only on head node
+		//		Some are executed both on head node and at remote tasks
+		//		Some are parsed on head node and executed only at remote tasks
 		return true;
 	}
 	
