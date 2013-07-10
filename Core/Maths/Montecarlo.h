dd77465939c0180db94c33ec8fe666e468995348
Keith Bugeja <keithbugeja@users.noreply.github.com>
2013-07-10 16:10:16 +0000

    ...

--- Core/Maths/Montecarlo.h
+++ Core/Maths/Montecarlo.h
@@ -17,6 +17,14 @@ namespace Illumina
 		class Montecarlo
 		{
 		public:
+			static Vector3 CosineSampleHemisphere(float p_u, float p_v)
+			{
+				float a = Maths::PiTwo * p_v,
+					b = Maths::Sqrt(p_u);
+
+				return Vector3(b * Maths::Cos(a), b * Maths::Sin(a), Maths::Sqrt(1 - p_u));
+			}
+
 			static Vector3 CosineSampleHemisphere(float p_u, float p_v, 
 				float p_j, float p_k, float p_m, float p_n)
 			{
