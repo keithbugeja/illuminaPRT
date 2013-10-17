231d2aa0c80bccd7149af40f01a81037d95e801e
Keith Bugeja <keithbugeja@users.noreply.github.com>
2013-10-17 10:57:06 +0000

    Illumina PRT - Shared memory compilation

--- Utility/Sandbox/main.cpp
+++ Utility/Sandbox/main.cpp
@@ -22,8 +22,8 @@
 //----------------------------------------------------------------------------------------------
 //	Set Illumina PRT compilation mode (SHM, DSM or P2P)
 //----------------------------------------------------------------------------------------------
-#define ILLUMINA_P2P
-// #define ILLUMINA_SHM
+// #define ILLUMINA_P2P
+#define ILLUMINA_SHM
 
 #if (!defined ILLUMINA_SHM) || (!defined ILLUMINA_P2P)
 	#define ILLUMINA_DSM
